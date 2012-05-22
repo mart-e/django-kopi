a7bff69710118c4c49c843b9db9af41ab6a90ce2
Martin Trigaux <me@mart-e.be>
2012-05-22 10:42:14 +0200

    new app

---
+++ kopi/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^kopi/', include(admin.site.urls)),
+    				   url(r'^static/(.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+)
+
