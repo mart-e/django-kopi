e4a1fc72bfbed3a602f2e5bb3ae344f904193cc2
Martin Trigaux <me@mart-e.be>
2012-05-24 17:47:44 +0200

    overwrite post-comment

--- comments/urls.py
+++ comments/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.contrib.comments.urls import urlpatterns
 
 
-urlpatterns += patterns('comments.views',
+urlpatterns = patterns('comments.views',
     url(r'^(?P<object_id>\d+)/edit/$',
         view='comment_edit',
         name='comments-edit'),
@@ -10,4 +10,11 @@ urlpatterns += patterns('comments.views',
     url(r'^(?P<object_id>\d+)/remove/$',
         view='comment_remove',
         name='comments-remove'),
+
+    # let's overwrite django.contrib.comments
+    url(r'^post/$',
+    	view='custom_comment_post',
+    	name='comments-post-comment'),
+
+    url( r'^', include( 'django.contrib.comments.urls' ) ),
 )
