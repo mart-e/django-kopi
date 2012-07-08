08d4311614a0d5d8c16bd687bf445329a9ce6822
Martin Trigaux <me@mart-e.be>
2012-07-08 14:04:12 +0200

    partial support of avatrs

--- comments/models.py
+++ comments/models.py
@@ -2,14 +2,37 @@ from django.db import models
 from django.contrib.comments.models import Comment
 
 from markdown import markdown
+import hashlib
+import requests
 
 class KopiComment(Comment):
     identifier = models.CharField(max_length=50)
     comment_html = models.TextField(editable=False, blank=True)
 
     def get_avatar_url(self):
-    	return "https://secure.gravatar.com/avatar/0c49d7e5a294cb2ca762eaede426b7a4?s=32&d=identicon&r=R"
+    	gravatar =  getGravatar(self.user_email)
+    	if gravatar:
+    		return gravatar
+
+
 
     def save(self, force_insert=False, force_update=False):
 		self.comment_html = markdown(self.comment, safe_mode=True)
 		super(KopiComment, self).save(force_insert, force_update)
+
+
+def getJavatar(url):
+	avatar = requests.get("{0}/avatar.png".format(url))
+	if avatar:
+		return avatar
+	else:
+		return False
+
+
+def getGravatar(email):
+	md5 = hashlib.md5(email).hexdigest()
+	avatar = requests.get("https://secure.gravatar.com/avatar/{0}".format(md5))
+	if avatar:
+		return avatar
+	else:
+		return False
